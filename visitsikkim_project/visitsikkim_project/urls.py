b7811e0f2533b3084c09507feda6e53c89b183a6
vishaldhara31 <vishaldhara31@gmail.com>
2026-01-04 21:30:22 +0530

    Refactor urls.py for clarity and consistency

--- visitsikkim_project/visitsikkim_project/urls.py
+++ visitsikkim_project/visitsikkim_project/urls.py
@@ -1,18 +1,17 @@
 
 from django.contrib import admin
-from django.urls import path, include   # ✅ include is needed here
-from django.conf import settings        # ✅ settings for serving media
-from django.conf.urls.static import static  # ✅ static() helper
+from django.urls import path, include  
+from django.conf import settings       
+from django.conf.urls.static import static  
 
 urlpatterns = [
-    path('admin/', admin.site.urls),   # ✅ admin URL works now
+    path('admin/', admin.site.urls),   
     path('', include("main.urls")), 
-    # path("", include("chatapp.urls")),
-    
-          # ✅ includes your app's urls
+   
 ]
 
-# Serve media files (images) during development
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
